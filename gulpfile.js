606f77fc55fd35950877d85a268c4ba2e63b040d
云游君 <me@yunyoujun.cn>
2019-01-30 20:43:07 +0800

    fix disabled input border

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,23 @@ var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
 var cssmin = require('gulp-cssmin')
 
-gulp.task('compile', function () {
-  return gulp.src('./src/*.scss')
+gulp.task('compile', function() {
+  return gulp
+    .src('./src/*.scss')
     .pipe(sass.sync())
-    .pipe(autoprefixer({
-      browsers: ['ie > 9', 'last 2 versions'],
-      cascade: false
-    }))
+    .pipe(
+      autoprefixer({
+        browsers: ['ie > 9', 'last 2 versions'],
+        cascade: false
+      })
+    )
     .pipe(cssmin())
     .pipe(gulp.dest('./lib'))
 })
 
-gulp.task('copyfont', function () {
-  return gulp.src('./src/fonts/**')
+gulp.task('copyfont', function() {
+  return gulp
+    .src('./src/fonts/**')
     .pipe(cssmin())
     .pipe(gulp.dest('./lib/fonts'))
 })
